30fb9e2f76748163a067a49e8d28556d735a6c86
Unicote <arvogl874@gmail.com>
2022-03-25 21:30:23 +0300

    gprivate: Import sysconfig for quick tap
    - Without this, our settings do not see it

--- gprivate.mk
+++ gprivate.mk
@@ -4,3 +4,7 @@
 PRODUCT_PACKAGES += \
     sysuia \
     settingsa \
+
+# Quick Tap
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/sysconfig/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
